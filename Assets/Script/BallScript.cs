a4ce22d057d23c0d2fb6084559e9f642da6ace41
TfromTYBros <kanshashitemasu358@gmail.com>
2022-03-16 01:17:58 +0900

    Barを9通りの変化が出来るように変更。変化をつけるのはboolから直接速度に変化をつけることで実装した、次はボールの初速を変える、

--- Assets/Script/BallScript.cs
+++ Assets/Script/BallScript.cs
@@ -15,8 +15,10 @@ public class BallScript : MonoBehaviour
     [SerializeField] bool Down = false;
 
     
-    float breakingValue = 1;
-    bool breaking = false;
+    float breakingXValue = 1;
+    float breakingYValue = 1;
+    //bool breakingX = false;
+    //bool breakingY = false;
 
     Vector3 BallStartPos = new Vector3(0.0f, -3.0f, -1.0f);
     WaitForSeconds countDownF = new WaitForSeconds(2.9f);
@@ -42,6 +44,7 @@ public class BallScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log("Hit");
+        //wall,block
         if (collision.transform.CompareTag("LeftRight"))
         {
             SetBallVectorChangeLeftRightHit();
@@ -50,36 +53,54 @@ public class BallScript : MonoBehaviour
         {
             SetBallVectorChangeUpBlockHit();
         }
-        if (collision.transform.CompareTag("BarLeft"))
+        //Bars
+        if (collision.transform.CompareTag("Bar0"))
         {
-            //Debug.Log("BarLeft");
-            SetBallVectorChangeBarLeft();
-            SetChangeBreaking();
+            Debug.Log("Bar0");
+            SetBallVectorChangeBar0Hit();
         }
-        if (collision.transform.CompareTag("BarLeftCenter"))
+        if (collision.transform.CompareTag("Bar1"))
         {
-            //Debug.Log("BarLeftCenter");
-            SetBallVectorChangeBarLeftCenter();
-            SetChangeBreaking();
+            Debug.Log("Bar1");
+            SetBallVectorChangeBar1Hit();
+        }
+        if (collision.transform.CompareTag("Bar2"))
+        {
+            Debug.Log("Bar2");
+            SetBallVectorChangeBar2Hit();
+        }
+        if (collision.transform.CompareTag("Bar3"))
+        {
+            Debug.Log("Bar3");
+            SetBallVectorChangeBar3Hit();
         }
         if (collision.transform.CompareTag("BarCenter"))
         {
-            //Debug.Log("BarCenter");
+            Debug.Log("BarCenter");
             SetBallVectorChangeBarCenterHit();
-            SetChangeBreaking();
         }
-        if (collision.transform.CompareTag("BarCenterRight"))
+        if (collision.transform.CompareTag("Bar5"))
         {
-            //Debug.Log("BarCenterRight");
-            SetBallVectorChangeBarCenterRightHit();
-            SetChangeBreaking();
+            Debug.Log("Bar5");
+            SetBallVectorChangeBar5Hit();
         }
-        if (collision.transform.CompareTag("BarRight"))
+        if (collision.transform.CompareTag("Bar6"))
         {
-            //Debug.Log("BarRight");
-            SetBallVectorChangeBarRightHit();
-            SetChangeBreaking();
+            Debug.Log("Bar6");
+            SetBallVectorChangeBar6Hit();
         }
+        if (collision.transform.CompareTag("Bar7"))
+        {
+            Debug.Log("Bar7");
+            SetBallVectorChangeBar7Hit();
+        }
+        if (collision.transform.CompareTag("Bar8"))
+        {
+            Debug.Log("Bar8");
+            SetBallVectorChangeBar8Hit();
+        }
+
+        //safeLine
         if (collision.transform.CompareTag("Catch"))
         {
             //Debug.Log("Catch");
@@ -123,48 +144,76 @@ public class BallScript : MonoBehaviour
 
     void VectorXPlus()
     {
-        BallVector.x += 0.025f * breakOut.GetLevel();
+        BallVector.x += (0.02f + breakingXValue) * breakOut.GetLevel();
     }
 
     void VectorXMinus()
     {
-        BallVector.x -= 0.025f * breakOut.GetLevel();
+        BallVector.x -= (0.02f + breakingXValue) * breakOut.GetLevel();
     }
 
     void VectorYPlus()
     {
-        BallVector.y += (0.025f * breakingValue) * breakOut.GetLevel();
+        BallVector.y += (0.02f + breakingYValue) * breakOut.GetLevel();
     }
 
     void VectorYMinus()
     {
-        BallVector.y -= (0.025f * breakingValue) * breakOut.GetLevel();
+        BallVector.y -= (0.02f + breakingYValue) * breakOut.GetLevel();
     }
 
-    void SetChangeBreaking()
+    void SetChangeBreakingX(float value)
     {
-        if (breaking) breakingValue = 1.5f;
-        else breakingValue = 1.0f;
+        breakingXValue = value;
     }
 
-    void SetBallVectorChangeBarLeft()
+    void SetChangeBreakingY(float value)
     {
-        //Debug.Log("SetBallChangeBarLeft");
+        breakingYValue = value;
+    }
+
+    void SetBallVectorChangeBar0Hit()
+    {
+        //Debug.Log("SetBallVectorChangeBarLeftEdgeHit");
         Right = false;
         Down = false;
         Left = true;
         Up = true;
-        breaking = false;
+        SetChangeBreakingX(0.02f);
+        SetChangeBreakingY(0.005f);
     }
 
-    void SetBallVectorChangeBarLeftCenter()
+    void SetBallVectorChangeBar1Hit()
     {
-        //Debug.Log("SetBallChangeBarLeftCenter");
+        //Debug.Log("SetBallChangeBarLeftHit");
         Right = false;
         Down = false;
         Left = true;
         Up = true;
-        breaking = true;
+        SetChangeBreakingX(0.015f);
+        SetChangeBreakingY(0.01f);
+    }
+
+    void SetBallVectorChangeBar2Hit()
+    {
+        //Debug.Log("SetBallChangeBar2");
+        Right = false;
+        Down = false;
+        Left = true;
+        Up = true;
+        SetChangeBreakingX(0.01f);
+        SetChangeBreakingY(0.015f);
+    }
+
+    void SetBallVectorChangeBar3Hit()
+    {
+        //Debug.Log("SetBallChangeBar3");
+        Right = false;
+        Down = false;
+        Left = true;
+        Up = true;
+        SetChangeBreakingX(0.005f);
+        SetChangeBreakingY(0.02f);
     }
 
     void SetBallVectorChangeBarCenterHit()
@@ -174,17 +223,50 @@ public class BallScript : MonoBehaviour
         Left = false;
         Down = false;
         Up = true;
-        breaking = false;
+        SetChangeBreakingX(0.0f);
+        SetChangeBreakingY(0.02f);
     }
 
-    void SetBallVectorChangeBarCenterRightHit()
+    void SetBallVectorChangeBar5Hit()
     {
-        //Debug.Log("SetBallVectorChangeBarCenterRight");
+        //Debug.Log("SetBallVectorChangeBar5Hit");
         Left = false;
         Right = true;
         Up = true;
         Down = false;
-        breaking = true;
+        SetChangeBreakingX(0.005f);
+        SetChangeBreakingY(0.02f);
+    }
+
+    void SetBallVectorChangeBar6Hit()
+    {
+        //Debug.Log("SetBallVectorChangeBar6Hit");
+        Left = false;
+        Right = true;
+        Up = true;
+        Down = false;
+        SetChangeBreakingX(0.01f);
+        SetChangeBreakingY(0.015f);
+    }
+    void SetBallVectorChangeBar7Hit()
+    {
+        //Debug.Log("SetBallVectorChangeBar7Hit");
+        Left = false;
+        Right = true;
+        Up = true;
+        Down = false;
+        SetChangeBreakingX(0.015f);
+        SetChangeBreakingY(0.01f);
+    }
+    void SetBallVectorChangeBar8Hit()
+    {
+        //Debug.Log("SetBallVectorChangeBar8Hit");
+        Left = false;
+        Right = true;
+        Up = true;
+        Down = false;
+        SetChangeBreakingX(0.02f);
+        SetChangeBreakingY(0.005f);
     }
 
     void SetBallVectorChangeBarRightHit()
@@ -194,7 +276,6 @@ public class BallScript : MonoBehaviour
         Right = true;
         Up = true;
         Down = false;
-        breaking = false;
     }
 
     public void BallPosReset()
@@ -210,7 +291,8 @@ public class BallScript : MonoBehaviour
         Left = false;
         Up = false;
         Down = false;
-        breaking = false;
+        SetChangeBreakingX(0.0f);
+        SetChangeBreakingY(0.0f);
     }
 
     public IEnumerator BallStartRightUp()
@@ -218,10 +300,10 @@ public class BallScript : MonoBehaviour
         yield return countDownF;
         Right = true;
         Up = true;
-
         Left = false;
         Down = false;
-        breaking = false;
+        SetChangeBreakingX(0.0f);
+        SetChangeBreakingY(0.0f);
     }
 
     public IEnumerator DebugStart()
